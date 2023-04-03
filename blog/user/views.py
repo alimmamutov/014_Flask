a79de3c3a3e80102e5c9924dbbe67548e8e3adee
alim <mamutov.creation@yandex.ru>
2023-04-03 23:59:45 +0300

    learning to create and use templates and render it in views or redirect on other urls. Worked with exceptions.

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,13 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect
+from werkzeug.exceptions import NotFound
 
 user = Blueprint('user', __name__, static_folder='../static', url_prefix='/users')
+
+USERS = {
+    1: 'Alice',
+    2: 'John',
+    3: 'Mike'
+}
 
 
 @user.route('/')
 def user_list():
-    return 'hello'
+    return render_template(
+        'users/list.html',
+        users=USERS
+    )
 
 
 @user.route('/<pk>')
 def get_user(pk: int):
-    return pk
+    pk = int(pk)
+    try:
+        user_name = USERS[pk]
+    except KeyError:
+        # raise NotFound(f'User id {pk} not found')
+        return redirect('https://www.yandex.ru/')  # redirected to another urls
+    return render_template(
+        'users/detail.html',
+        user_name=user_name
+    )
