2ac15d798ff481bc627fd10eab4422dc03d42049
alim <mamutov.creation@yandex.ru>
2023-04-04 23:25:51 +0300

    learn how to use static created template for reports

--- blog/report/views.py
+++ blog/report/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 report = Blueprint('report', __name__, static_folder='../static', url_prefix='/reports')
 
 
 @report.route('/')
 def report_list():
-    return 'hello rep'
+    return render_template('reports/list.html', reports=['report1', 'report2', 'report3', 'report4'])
